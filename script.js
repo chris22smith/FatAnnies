73df6acd63e25323183a18a60d00fd990951aad7
Chris Smith <chris22smith@gmail.com>
2021-07-04 20:43:22 +0100

    inline script

--- script.js
+++
@@ -1,6 +0,0 @@
-const now = new Date();
-
-if (now.getHours() >= 17) {
-    const orderButton = document.querySelector('#order-button');
-    orderButton.setAttribute('href', 'https://www.ubereats.com/gb/store/fat-annies/1upHmJUbQWueLGzeMAbPTA');
-}
